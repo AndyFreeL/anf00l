968c7ff74709b0243330993991f81ff519e432ab
AndyFreeL <andy00freelance@gmail.com>
2023-06-06 19:47:45 +0700

    add work

--- src/works.js
+++ src/works.js
@@ -7,15 +7,18 @@ import cyberia from './assets/images/works/cyberia.png'
 import depocalc from './assets/images/works/depocalc.png'
 import drawlines from './assets/images/works/drawlines.png'
 import mria from './assets/images/works/mriaresort.png'
+import currencyCalc from './assets/images/works/currencyCalc.png'
+
 
 export default [
-    {id:1, description:'Лендинг, верстка', img:minim, link:'https://andyfreel.github.io/minim/'},
-    {id:2, description:'Лендинг, верстка', img:startup, link:'https://andyfreel.github.io/startup/'},
-    {id:3, description:'Лендинг, верстка', img:single, link:'https://andyfreel.github.io/single_page/'},
+    {id:1, description:'Карточки с курсами валют и графики React', img:currencyCalc, link:'https://currency-rate-pied.vercel.app/'},
+    {id:2, description:'Калькулятор депозита для скальпера React', img:depocalc, link:'https://andyfreel.github.io/deposit-calculator/'},
+    {id:3, description:'Страница сайта React, Next.js', img:cyberia, link:'https://cyberia-test-fwbi526fv-andyfreel.vercel.app/projects'},
     {id:4, description:'Лендинг, верстка', img:adn, link:'https://andyfreel.github.io/ADN-test/'},
-    {id:5, description:'React, Next.js', img:cyberia, link:'https://cyberia-test-fwbi526fv-andyfreel.vercel.app/projects'},
-    {id:6, description:'React', img:depocalc, link:'https://andyfreel.github.io/deposit-calculator/'},
+    {id:5, description:'Лендинг, верстка', img:mria, link:'https://andyfreel.github.io/mriyaresort/'},
+    {id:6, description:'Vue, игра', img:simon, link:'https://andyfreel.github.io/simonGameVue/'},
     {id:7, description:'Vue, игра', img:drawlines, link:'https://andyfreel.github.io/draw-lines/'},
-    {id:8, description:'Лендинг, верстка', img:mria, link:'https://andyfreel.github.io/mriyaresort/'},
-    {id:9, description:'Vue, игра', img:simon, link:'https://andyfreel.github.io/simonGameVue/'}
+    {id:8, description:'Лендинг, верстка', img:minim, link:'https://andyfreel.github.io/minim/'},
+    {id:9, description:'Лендинг, верстка', img:startup, link:'https://andyfreel.github.io/startup/'},
+    {id:10, description:'Лендинг, верстка', img:single, link:'https://andyfreel.github.io/single_page/'},
 ]
