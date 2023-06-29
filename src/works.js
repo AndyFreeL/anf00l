1234c0c449a7aa43e083040c880a2a97cae34e07
AndyFreeL <andy00freelance@gmail.com>
2023-06-29 17:36:12 +0700

    Add works

--- src/works.js
+++ src/works.js
@@ -8,16 +8,18 @@ import depocalc from './assets/images/works/depocalc.png'
 import drawlines from './assets/images/works/drawlines.png'
 import mria from './assets/images/works/mriaresort.png'
 import currencyCalc from './assets/images/works/currencyCalc.png'
+import cloudForm from './assets/images/works/cloudForm.jpg'
+import teaStore from './assets/images/works/teaStore.jpg'
 
 
 export default [
     {id:1, description:'Карточки с курсами валют и графики React', img:currencyCalc, link:'https://currency-rate-pied.vercel.app/'},
     {id:2, description:'Калькулятор депозита для скальпера React', img:depocalc, link:'https://andyfreel.github.io/deposit-calculator/'},
+    {id:6, description:'Чайный магазин React, Express, PgSQL', img:teaStore, link:'https://tea-store.vercel.app/'},
+    {id:6, description:'Форма Cloud.ru React', img:cloudForm, link:'https://cloud-form-two.vercel.app/'},
     {id:3, description:'Страница сайта React, Next.js', img:cyberia, link:'https://cyberia-test-fwbi526fv-andyfreel.vercel.app/projects'},
     {id:4, description:'Лендинг, верстка', img:adn, link:'https://andyfreel.github.io/ADN-test/'},
     {id:5, description:'Лендинг, верстка', img:mria, link:'https://andyfreel.github.io/mriyaresort/'},
-    {id:6, description:'Vue, игра', img:simon, link:'https://andyfreel.github.io/simonGameVue/'},
-    {id:7, description:'Vue, игра', img:drawlines, link:'https://andyfreel.github.io/draw-lines/'},
     {id:8, description:'Лендинг, верстка', img:minim, link:'https://andyfreel.github.io/minim/'},
     {id:9, description:'Лендинг, верстка', img:startup, link:'https://andyfreel.github.io/startup/'},
     {id:10, description:'Лендинг, верстка', img:single, link:'https://andyfreel.github.io/single_page/'},
